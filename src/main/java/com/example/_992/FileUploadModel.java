dfe1e1f612034b18e11ea46e13efc7f4638adc05
Elan Olenja <elan.olenja@elans-air.mynetworksettings.com>
2024-09-18 23:14:02 -0400

    first commit

---
+++ src/main/java/com/example/_992/FileUploadModel.java
@@ -0,0 +1,11 @@
+package com.example._992;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
